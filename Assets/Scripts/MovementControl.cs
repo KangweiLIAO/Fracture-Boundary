4b32f1e7db05e882ed7e6a0b9a7fe4f643ac780a
KangweiPC <Clanceyliao@gmail.com>
2020-01-21 20:39:00 -0500

    Movement enhanced

--- Assets/Scripts/MovementControl.cs
+++ Assets/Scripts/MovementControl.cs
@@ -4,23 +4,31 @@ using UnityEngine;
 
 public class MovementControl : MonoBehaviour
 {
-    public int speed = 1;
-    public int rotateSpeed = 3;
+    // public vars
+    public int walkSpeed = 1;
+    public int rotateSpeed = 4;
+    public int jumpSpeed = 100;
+    public float gravity = 9;
+    public bool walking;
+    public bool running;
+    public bool idling;
+    public bool jumping;
+    public bool backrunning;
 
+    // private vars
+
+
+    // components
     private Animator animator;
     private CharacterController controller;
     private Vector3 moveDirection;
-    private  float gravity;
-    private bool walking;
-    private bool running;
-
-    private int directionState;
-    private int lastDirectionState;
 
     // Start is called before the first frame update
     void Start()
     {
-        gravity = 9;
+        idling = true;
+        walking = running = false;
+
         animator = GetComponent<Animator>();
         controller = GetComponent<CharacterController>();
     }
@@ -33,78 +41,99 @@ public class MovementControl : MonoBehaviour
 
     void Movement()
     {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
         if (controller.isGrounded)
         {
-            if (Input.GetKeyDown(KeyCode.W))
+            transform.Rotate(0, horizontalInput * rotateSpeed, 0);
+            moveDirection = new Vector3(horizontalInput, 0, verticalInput);
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= walkSpeed;
+
+            // Run
+            if (Input.GetKeyDown(KeyCode.LeftShift))
             {
-                if (running) animator.SetTrigger("Run01");
-                else
-                {
-                    animator.SetTrigger("Walk01");
-                    walking = true;
-                }
-            } else if (Input.GetKeyUp(KeyCode.W)) {
-                if (!running) animator.SetTrigger("Idle01");
-                walking = false;
+                walkSpeed = 3;
+                setAllMovement(false);
+                running = true;
+            } else if (Input.GetKeyUp(KeyCode.LeftShift)) {
+                walkSpeed = 1;
+                running = false;
             }
 
+            // Run back
             if (Input.GetKeyDown(KeyCode.S))
             {
-                speed = 2;
-                walking = false;
-                animator.SetTrigger("RunBack01");
-            } else if (Input.GetKeyUp(KeyCode.S)) {
-                speed =  1;
-                if (!running) animator.SetTrigger("Idle01");
+                walkSpeed = 2;
+                setAllMovement(false);
+                backrunning = true;
+            }
+            else if (Input.GetKeyUp(KeyCode.S)) backrunning = false;
+
+            // Jump
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                moveDirection.y += jumpSpeed;
+                setAllMovement(false);
+                jumping = true;
+            }
+            else if (Input.GetKeyUp(KeyCode.Space)) jumping = false;
+
+            // Walking detection
+            if ((Mathf.Abs(horizontalInput) >= 0.01 || Mathf.Abs(verticalInput) >= 0.01) && (!running && !backrunning && !jumping))
+            {
+                setAllMovement(false);
+                walking = true;
+            }
+            if (Mathf.Abs(horizontalInput) <= 0.1 && Mathf.Abs(verticalInput) <= 0.1)
+            {
+                setAllMovement(false);
+                idling = true;
             }
 
-            if (Input.GetKeyDown(KeyCode.A))
+            // Animation controls:
+            StopAllAnimation();
+            if (idling)
             {
-                if (running) animator.SetTrigger("Run01");
-                else
-                {
-                    animator.SetTrigger("Walk01");
-                    walking = true;
-                }
-            } else if (Input.GetKeyUp(KeyCode.A)) {
-                if (!running) animator.SetTrigger("Idle01");
-                walking = false;
+                animator.SetBool("Idle1", true);
             }
-
-            if (Input.GetKeyDown(KeyCode.D))
+            if (walking)
             {
-                if (running) animator.SetTrigger("Run01");
-                else
-                {
-                    animator.SetTrigger("Walk01");
-                    walking = true;
-                }
-            } else if (Input.GetKeyUp(KeyCode.D)) {
-                if (!running) animator.SetTrigger("Idle01");
-                walking = false;
+                animator.SetBool("Walk1", true);
             }
-
-            if (Input.GetKeyDown(KeyCode.LeftShift) && walking) 
+            if (running)
             {
-                if (!Input.GetKeyDown(KeyCode.S))
-                {
-                    speed = 3;
-                    running = true;
-                    walking = false;
-                    animator.SetTrigger("Run01");
-                }
-            }else if (Input.GetKeyUp(KeyCode.LeftShift)) {
-                if (!Input.GetKeyDown(KeyCode.S))
-                {
-                    speed = 1;
-                    running = false;
-                    animator.SetTrigger("Idle01");
-                }
+                animator.SetBool("Run1", true);
+            }
+            if (backrunning)
+            {
+                animator.SetBool("Runback1", true);
+            }
+            if (jumping)
+            {
+                animator.SetBool("Jump1", true);
             }
         }
-        moveDirection = new Vector3(Input.GetAxis("Horizontal") * speed, 0, Input.GetAxis("Vertical") * speed);
-        moveDirection = transform.TransformDirection(moveDirection);
+        moveDirection.y -= gravity * Time.deltaTime;     //gravity
         controller.Move(moveDirection * Time.deltaTime);
-        transform.Rotate(0, Input.GetAxis("Horizontal") * rotateSpeed, 0);
+    }
+
+    void setAllMovement(bool state)
+    {
+        walking = state;
+        running = state;
+        idling = state;
+        jumping = state;
+        backrunning = state;
+    }
+
+    void StopAllAnimation()
+    {
+        animator.SetBool("Walk1", false);
+        animator.SetBool("Run1", false);
+        animator.SetBool("Runback1", false);
+        animator.SetBool("Idle1", false);
+        animator.SetBool("Jump1", false);
     }
 }
